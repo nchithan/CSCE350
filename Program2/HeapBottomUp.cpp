9d058d448ba2fbfd333030623e3ce01d9b59eb88
Naveen Chithan <chithannaveen@gmail.com>
2022-03-29 21:24:41 -0400

    HeapSort

--- Program2/HeapBottomUp.cpp
+++ Program2/HeapBottomUp.cpp
@@ -39,62 +39,72 @@ void getInput(string filename, vector<int> *list){
 
 }
 
-// void heapSort(vector<int> *list, vector<int> *list2){
-//     int n = (int) (*list).size();
-//     n = n-1;
-//     for(int i = n, j =0; i > 0; i--, j++){
-//         swep((*&list), i);
-//         (*list2).push_back((*list).at(i));
-//         (*list).pop_back();
-//         heapify(*&list,i);
-//     }
-// }
 
-// void swep(vector<int> *list, int index2){
-//     std::iter_swap((*list).begin(), (*list).begin()+ index2);
-//  }
 
-void heapify(vector<int> *list, int n){
-    //cout << "size n: " << n << endl;
+void swep(vector<int> &list, int index2){
+    std::iter_swap((list).begin(), (list).begin()+ index2);
+ }
+
+void heapify(vector<int> &list, int n){
+    // cout << "size n: " << n << endl;
     for(int i = n/2; i >= 0; i=i-1){
         int k = i;
-        //cout << "Outside i: " << i << endl;
-        int v = (*list).at(k);
-        //cout << "Outside v: " << v << endl;
+        // cout << "Outside i: " << i << endl;
+        int v = (list).at(k);
+        // cout << "Outside v: " << v << endl;
         bool heap = false;
         while(!heap && 2*k <= n){
             int j = 2*k;
-            //cout << "Inside j: " << j << endl;
-            //cout << "Inside i: " << i << endl;
+            // cout << "Inside j: " << j << endl;
+            // cout << "Inside i: " << i << endl;
             if(j < n){
-                //cout << "Inside First if: " << j << endl;
-                if((*list).at(j) < (*list).at(j+1)){
+                // cout << "Inside First if: " << j << endl;
+                if((list).at(j) < (list).at(j+1)){
                     j = j+1;
-                    //cout << "Inside Nested if: " << j << endl;
+                    // cout << "Inside Nested if: " << j << endl;
                 }
             }
-            if(v >= (*list).at(j)){
-                //cout << "Inside Second if: " << j << endl;
+            if(v >= (list).at(j)){
+                // cout << "Inside Second if: " << j << endl;
                 heap = true;
             } else {
-                (*list).at(k) = (*list).at(j);
+                (list).at(k) = (list).at(j);
                 k = j;
-                //cout << "Inside Else : " << k << endl;
+                // cout << "Inside Else : " << k << endl;
             }
         }
-        (*list).at(k) = v;
+        (list).at(k) = v;
     }
 }
 
-void printOutput(vector<int> *list, vector<int> *list2){
+void heapSort(vector<int> &list, vector<int> *list2){
+    int n = (int) (list).size();
+    n = n-1;
+    for(int i = n, j =0; i > 0; i--, j++){
+        swep((list), i);
+        // cout << "n: " << n << endl;
+        // cout << "i: " << i << endl;
+        (*list2).push_back((list).at(i));
+        // cout << "after i: " << i << endl;
+        (list).pop_back();
+        // cout << "after2 i: " << i << endl;
+        heapify(list,i-1);
+        // cout << "after3 i: " << i << endl;
+    }
+    (*list2).push_back((list).at(0));
+}
+
+void printOutput(vector<int> &list){
     const string filename = "output.txt";
     ofstream file(filename);
-    for(unsigned i = 0; i<(*list).size(); i++){ 
-        file << (*list).at(i) << " ";
+    for(unsigned i = 0; i<(list).size(); i++){ 
+        file << (list).at(i) << " ";
     }
-    cout << endl;
-    for(unsigned i = 0; i<(*list2).size(); i++){ 
-        file << (*list2).at(i) << " ";
+    file << endl;
+    vector<int> list2;
+    heapSort(list, &list2);
+    for(unsigned i = 0; i<(list2).size(); i++){ 
+        file << (list2).at(i) << " ";
     }
     file.close();
 }
@@ -120,13 +130,13 @@ int main(int argc, char** argv){
     int n = (int) list.size();
     n = n-1;
     gettimeofday(&start, NULL);
-    heapify(&list, n);
+    heapify(list, n);
     gettimeofday(&end, NULL);
     secs  = end.tv_sec  - start.tv_sec;
     usecs = end.tv_usec - start.tv_usec;
     mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
     cout << "Execution time: " << mtime << " milliseconds"<< endl;
     //heapSort(&list, &list2);
-    printOutput(&list, &list2);
+    printOutput(list);
     return 0;
 }
